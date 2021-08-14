2d42cef4dce823b0a421583ee68ca26e3c63a724
talysonxx <talysonoficial12@gmail.com>
2021-08-14 12:58:30 -0300

    código encurtado, rápida explicação dos termos e autocomplete melhorado

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 let map
 // inicar mapa
 function initMap() {
+    // adiciona o mapa, primeiro parâmetro local onde vai ser adicionado, no segundo um objeto com as configurações do para, como localização, zoom, título
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -3.8168435203245368, lng: -49.66447686294523},
         /*
@@ -15,10 +16,6 @@ function initMap() {
         title: 'Tucuruí - PA'
     })
 
-
-
-
-
     // autocompletar
     let lugar = document.getElementById('lugar')
     let autocomplete
@@ -31,20 +28,19 @@ function initMap() {
         }
     )
 
-
-
-
-    // marcar (tentativa)
-    // autocomplete.addEventListener('place_changed', () => {
-    //     const place = autocomplete.getPlace()
-    //     new google.maps.Marker(
-    //         {
-    //             position: place.geometry.location,
-    //             title: place.name,
-    //             map: map,
-    //         }
-    //     )
-    // })
+    // sempre que o local do input for mudado, ele adiciona um marker e move o mapa para a localização do mesmo
+    autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace()
+        // new google.maps.Marker(
+        //     {
+        //         position: place.geometry.location,
+        //         title: place.name,
+        //         map: map,
+        //     }
+        // )
+        adicionarMarker(place.geometry.location, place.name)
+        map.panTo(place.geometry.location)
+    })
 
 
     
@@ -69,14 +65,40 @@ function initMap() {
         }
     ]
     const image = 'https://cdn.discordapp.com/attachments/799379659347066922/875823943372664902/image-100px.jpg'
+
+    // adicionar um marker sempre que chamado. Apesar de que no caso de ser chamado atráves do forEach o parâmetro latLng recebe um obj com mais de dois atributos, ele só pega os dois primeiros (lat e lng)
+    function adicionarMarker(latLng, title){
+        new google.maps.Marker({
+            position: latLng,
+            map,
+            title,
+            icon: image
+        })
+    }
+
+    // vai passando por todos os 'lugares' do array, pegando sua localização e passando como parâmetro na função adicionarMarker 
     lugares.forEach(lugar => {
-        new google.maps.Marker(
-            {
-                position: lugar,
-                map,
-                title: lugar.title,
-                icon: image
-            }
-        )
+        // new google.maps.Marker(
+        //     {
+        //         position: lugar,
+        //         map,
+        //         title: lugar.title,
+        //         icon: image
+        //     }
+        // )
+        adicionarMarker(lugar, lugar.title)
     })
+    // quando o mapa for clicado, chama a função adicionarMarker passando o primeiro parâmetro como a localização (lat e long) e o titulo da marcação
+    let cont = 1
+    map.addListener('click', function(event){
+        adicionarMarker(event.latLng, `Marcação ${cont}`)
+        cont++
+    })
+
+
+    // eventos
+    // propriedade_change; zoom_change
+    // map.addListener('zoom_changed', function(){
+    //     console.log('aoooba')
+    // })
 }
